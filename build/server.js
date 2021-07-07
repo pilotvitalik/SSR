c6010da3600ef3d899be992539b67c6e69cf2da4
Vitaliy <itvkip@yandex.ru>
2021-07-07 22:34:08 +0300

    Added request for data about share

--- build/server.js
+++ build/server.js
@@ -1,14 +1,29 @@
 require('dotenv').config({path: '../.env'});
 const http = require('http');
+const axios = require('axios');
 
 const hostname = process.env.HOSTNAME;
 const port = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hi piece!');
+	if (req.url !== '/') return false;
+	sendReq(res);
 });
+
+async function sendReq(res){
+	let obj = {};
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'application/json');
+	try {
+		const response = await axios.get(process.env.DATA_SHARE_SINGLE);
+		obj.columns = response.data.boards.columns;
+		obj.data = response.data.boards.data
+		res.end(JSON.stringify(obj));
+	} catch (err) {
+		res.end(JSON.stringify(err));
+	}
+}
+
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
