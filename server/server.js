605bd19786066b3c6e93b4d6052264187ec001bb
Vitaliy <12345678t>
2021-07-09 17:00:23 +0300

    Добавлено подключение к БД

--- server/server.js
+++ server/server.js
@@ -1,27 +1,38 @@
 require('dotenv').config();
 const http = require('http');
 const axios = require('axios');
+const mysql = require('mysql2');
 
 const hostname = process.env.HOSTNAME;
 const port = process.env.PORT;
+
+const connection = mysql.createConnection({
+	host: process.env.HOST_DB,
+	user: process.env.USER,
+	database: process.env.DB,
+	password: process.env.PASWD
+});
 
 const server = http.createServer((req, res) => {
 	if (req.url === '/favicon.ico') return false;
 	sendReq(res);
 });
 
-async function sendReq(res){
-	let obj = {};
+async function sendReq(res) {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
-	try {
-		const response = await axios.get(process.env.DATA_SHARE_SINGLE);
-		obj.columns = response.data.boards.columns;
-		obj.data = response.data.boards.data
-		res.end(JSON.stringify(obj));
-	} catch (err) {
-		res.end(JSON.stringify(err));
-	}
+		connection.query(
+			'describe `route`',
+		function (err, results, fields) {
+			res.end(JSON.stringify(results));
+			if (err) {
+				console.log('ERROR');
+				console.log(results);
+			}
+		}
+
+	)
+		;
 }
 
 
